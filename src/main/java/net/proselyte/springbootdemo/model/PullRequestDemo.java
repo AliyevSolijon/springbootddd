d60850461b435e75584004174422090c95647bda
Solijon <Solijon.aliyev@mail.ru>
2022-10-21 18:23:39 +0500

    komment 2 qator

--- src/main/java/net/proselyte/springbootdemo/model/PullRequestDemo.java
+++ src/main/java/net/proselyte/springbootdemo/model/PullRequestDemo.java
@@ -3,4 +3,5 @@ package net.proselyte.springbootdemo.model;
 public class PullRequestDemo {
     //GitHub da PullRequest ni test qilish uchun yaratdim bu faylni
     //download qildim yana qaytarib joyladim
+    //xammasi yaxshi ishladi
 }
