7db20981d1d711d474cd1b1596ed52d189bfa222
Solijon <Solijon.aliyev@mail.ru>
2022-10-21 17:59:03 +0500

    komment

--- src/main/java/net/proselyte/springbootdemo/model/PullRequestDemo.java
+++ src/main/java/net/proselyte/springbootdemo/model/PullRequestDemo.java
@@ -2,4 +2,5 @@ package net.proselyte.springbootdemo.model;
 
 public class PullRequestDemo {
     //GitHub da PullRequest ni test qilish uchun yaratdim bu faylni
+    //download qildim yana qaytarib joyladim
 }
